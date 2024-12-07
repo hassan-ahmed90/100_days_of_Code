1ae1eaa28f53a67431474a59e3e810d32db3cb29
Jawad <jawadsoomro183@gmail.com>
2024-12-07 09:19:21 -0800

    First Commit

---
+++ Day02/bmi_calculator.py
@@ -0,0 +1,11 @@
+bmi = 84 / 1.65 ** 2
+print(bmi)
+print(int(bmi))
+print(round(bmi))
+print(round(bmi,2))
+
+score=12
+height=5.6
+isWinning=False
+score+=1
+print(f"Your score is = {score} and height is {height} and you have lost {isWinning}")
