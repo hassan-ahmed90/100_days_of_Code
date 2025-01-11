417271846316a3fd5a64f0279375f01840bdb432
Hassan Ahmed <hassanahmed40005@gmail.com>
2025-01-11 22:52:33 +0500

    29th day

---
+++ Day_29/main.py
@@ -0,0 +1,44 @@
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def save_password():
+    print()
+# ---------------------------- UI SETUP ------------------------------- #
+
+from tkinter import *
+
+window =Tk()
+window.title("Password Manager")
+window.config(padx=20,pady=20)
+
+canvas =Canvas(height=200,width=200)
+imagesrc=PhotoImage(file="logo.png")
+canvas.create_image(100,100,image=imagesrc)
+canvas.grid(row=0,column=1)
+
+#Labels
+website_label=Label(text="Website")
+website_label.grid(row=1,column=0)
+email_label=Label(text="Email/Username")
+email_label.grid(row=2,column=0)
+password_label= Label(text="Password")
+password_label.grid(row=3,column=0)
+
+#Entries
+website_entry=Entry(width=35)
+website_entry.grid(row=1,column=1,columnspan=2)
+website_entry.focus()
+
+email_entry=Entry(width=35)
+email_entry.grid(row=2,column=1,columnspan=2)
+email_entry.insert(0,"hassanahmedrajput@gmail.com ")
+password_entry=Entry(width=18)
+password_entry.grid(row=3,column=1)
+
+#Button
+generate_button=Button(width=14,text="Generate Password")
+generate_button.grid(row=3,column=2)
+add_button=Button(text="Add",width=36,command=save_password)
+add_button.grid(row=4,column=1,columnspan=2)
+window.mainloop()
