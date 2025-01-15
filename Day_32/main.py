f53345f4226e94f1eef5b0ac841663c8f01d19c7
Hassan Ahmed <hassanahmed40005@gmail.com>
2025-01-15 15:38:58 +0500

    32th day

--- Day_32/main.py
+++ Day_32/main.py
@@ -1,6 +1,13 @@
-import smtpd
 import smtplib
 
 
-my_email="hassanahmedrajput@gmail.com"
-connection=smtplib.SMTP("smtp.gmail.com")
+my_email="hassanahmed40005@gmail.com"
+my_password="cwjjrouxxbjkvzam"
+with smtplib.SMTP("smtp.gmail.com",587) as connection:
+    connection.starttls()
+    connection.login(user=my_email,password=my_password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="hassanahmedrajput@gmail.com",
+        msg="Subject:Hello Python SMPT \n\n\n This is the messege that i have learned Python 100 days"
+    )
